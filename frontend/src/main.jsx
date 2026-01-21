34e748cb445eed5ef6e5d5cf9cdcdd12aa284b54
AbhayKartik <AbhayKartik9099@gmail.com>
2026-01-21 21:07:44 +0530

    Routes Done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { AuthProvider } from './authContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>,
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </AuthProvider>
 )
